bf6b2d2390d6873e74eec03caa8c88e73d110d09
IamLizu <thegeek@iamlizu.com>
2020-08-22 00:42:30 +0600

    added app header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Items from './components/Items';
+import styles from './App.module.css'
 
 function App() {
   return (
     <>
+      <h2 className={styles.header}>
+        My Recipe App
+      </h2>
       <Items />
     </>
   );
